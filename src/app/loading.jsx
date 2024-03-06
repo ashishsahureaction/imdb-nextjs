5797f721cd07e31cbe3777e557220e0511c4fb84
ashishsahureaction <148655100+ashishsahureaction@users.noreply.github.com>
2024-03-06 09:31:20 -0500

    first commit

---
+++ src/app/loading.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+
+export default function loading() {
+  return (
+    <div className='flex justify-center mt-16'>
+      <Image className='h-52' src='spinner.svg' alt='loading...' width=
+      {30} height={30} />
+    </div>
+  );
+}
